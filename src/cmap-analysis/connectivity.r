5985f037cf282e911fd3e0158f96f63937e21f16
D. R. Mani <manidr@broadinstitute.org>
2018-08-21 17:37:42 -0400

    Enrichment calculation revision to match old method

--- src/cmap-analysis/connectivity.r
+++ src/cmap-analysis/connectivity.r
@@ -21,13 +21,12 @@ Source ('gct-io.r')
 pacman::p_load (dplyr)
 
 
-cmap.connectivity <- function (subset.scores.dir, results.prefix, tau.threshold=90, threshold.count=4) {
+cmap.connectivity <- function (subset.scores.dir, results.prefix, rankpt.n=4, mean.rankpt.threshold=90) {
   ## calculate CMAP connectivity scores after assembling subset ssGSEA (un-normalized ES) scores into a single table
   ## determine significant genes: 
   ##   a gene has a significant effect if it is CIS-enriched
-  ##   enrichment is defined as at least threshold.count profiles having tau > tau.threshold
-  #     (defaults for tau.threshold and threshold.count are set to mimic the mean_rankpt_4 metric)
-  
+  ##   enrichment is defined as mean.rankpt<n> > mean.rankpt.threshold
+
   ES <- NULL
   for (f in dir (subset.scores.dir, pattern='-scores\\.gct$')) {
     d <- parse.gctx (file.path (subset.scores.dir, f))
@@ -47,7 +46,8 @@ cmap.connectivity <- function (subset.scores.dir, results.prefix, tau.threshold=
   
   
   # See Subramanian et. al., Cell 171, 1437-1452. 2017.
-  # (for normalized scores and connectivity scores calculated below)
+  # (for normalized scores and connectivity scores calculated below; rankpt's are similar to
+  #  perturbagen centric measure of connectivity)
   
   # normalized ES (p,c,s) = ES (p,c,s) / mean ( ES(p,c,s) ) where
   #  p=perturbation type
@@ -56,6 +56,7 @@ cmap.connectivity <- function (subset.scores.dir, results.prefix, tau.threshold=
   # i.e., scores are normalized within each perturbation type and cell line,  
   # separately for +ve and -ve scores
   # NB: in dplyr, a formula is converted to a function; for a single argument to the function, use .
+  #     group_by groups rows, and mutate_all transforms each column (by row groups)
   nes <- es %>% group_by (ES@cdesc$pert_type, ES@cdesc$cell_id)  %>% 
     mutate_all (~ ifelse(. > 0, . / mean (.[.>0], na.rm=T), . / mean (abs (.[.<=0]), na.rm=T)))
   nes <- nes [, 1:ncol(es)]  # remove grouping columns at the end (added by dplyr)
@@ -63,34 +64,42 @@ cmap.connectivity <- function (subset.scores.dir, results.prefix, tau.threshold=
   colnames (NES) <- colnames (ES@mat)
   write.score (NES, sprintf ("%s-nes.gct", results.prefix))
   
-  # connectivity map score (tau)
+  # connectivity map score (tau) -- not used in calculating rankpt's (ala old scoring method)
   # standarized NES for each signature (ie., sample or id), using current set of queries as compendium
-  tau <- nes %>% mutate_all (~ sign (.) * 100 / length(.) * sapply (., function (x) sum (abs(.) < abs(x))) )
-  tau.t <- t (tau)
-  colnames (tau.t) <- colnames (ES@mat)
-  write.score (tau.t, sprintf ("%s-connectivity.gct", results.prefix))
+  ## tau <- NES %>% mutate_all (~ sign (.) * 100 / length(.) * sapply (., function (x) sum (abs(.) < abs(x))) )
+
+  # connectivity map rank-point scores -- score percentile NES over all perturbations/cell-lines for each query (== gene set)
+  rankpts <- nes %>% mutate_all (~ sign (.) * 100 / length(.) * sapply (., function (x) sum (abs(.) < abs(x))) )
+  rankpts.t <- t (rankpts)
+  colnames (rankpts.t) <- colnames (ES@mat)
+  write.score (rankpts.t, sprintf ("%s-connectivity.gct", results.prefix))
   
-  # summarize TAU scores for each gene by counting the number of tau's > tau.threshold
-  # take into account +ve and -ve extremes, and penalize for mixed extremes in a given gene
-  sig.counts <- tau %>% group_by (gene=ES@cdesc$pert_iname) %>% summarise_all (~ sum (. > tau.threshold) - sum (. < -tau.threshold))
-  write.csv (sig.counts, sprintf ("%s-sigcounts.csv", results.prefix), row.names=FALSE)
+  # determine mean rankpt_n -- mean of (best of +ve / -ve) n rank points
+  mean.rankpt <- function (x, n=rankpt.n) {
+    sx <- sort (x)
+    m.lo <- mean ( sx[1:n], na.rm=TRUE )
+    m.hi <- mean ( rev(sx)[1:n], na.rm=TRUE )
+    ifelse (abs(m.hi) >= abs(m.lo), m.hi, m.lo)
+  }
+  mean_rankpt.n <- rankpts %>% group_by (gene=ES@cdesc$pert_iname) %>% summarise_all (~ mean.rankpt (.))
+  write.csv (mean_rankpt.n, sprintf ("%s-meanrankpt%d.csv", results.prefix, rankpt.n), row.names=FALSE)
   # a gene is significant if it is CIS-enriched in both CNAdel (+ve) and CNAamp (-ve)
-  # genes uses for the analysis are listed in the *-gene-list.csv file output by cmap-input.r
+  # genes used for the analysis are listed in the *-gene-list.csv file output by cmap-input.r
   genes <- read.csv ( sprintf ("%s-gene-list.csv", results.prefix), as.is=TRUE )[,1]
   sig.amp <- sig.del <- sig.both <- NULL
-  sig.cols <- colnames (sig.counts)
+  sig.cols <- colnames (mean_rankpt.n)
   for (g in genes) {
     print (g)
-    g.row <- which (sig.counts [,'gene'] == g)
-    if (length (g.row) != 1) next   # gene not found in sig.counts table
+    g.row <- which (mean_rankpt.n [,'gene'] == g)
+    if (length (g.row) != 1) next   # gene not found in mean_rankpt.n table
     amp.col <- sprintf ("%s_CNAamp", g)
-    amp <- ifelse (amp.col %in% sig.cols, sig.counts [g.row, amp.col], 0)
+    amp <- ifelse (amp.col %in% sig.cols, mean_rankpt.n [g.row, amp.col], 0)
     del.col <- sprintf ("%s_CNAdel", g)
-    del <- ifelse (del.col %in% sig.cols, sig.counts [sig.counts [,'gene'] == g, del.col], 0)
-    if (amp <= -threshold.count && del >= threshold.count) { sig.both <- c (sig.both, g) }
+    del <- ifelse (del.col %in% sig.cols, mean_rankpt.n [mean_rankpt.n [,'gene'] == g, del.col], 0)
+    if (amp <= -mean.rankpt.threshold && del >= mean.rankpt.threshold) { sig.both <- c (sig.both, g) }
     else {
-      if (amp <= -threshold.count) sig.amp <- c (sig.amp, g)
-      if (del >= threshold.count) sig.del <- c (sig.del, g)
+      if (amp <= -mean.rankpt.threshold) sig.amp <- c (sig.amp, g)
+      if (del >= mean.rankpt.threshold) sig.del <- c (sig.del, g)
     }
   }
   write.table (sig.both, sprintf ("%s-sig-genes-bidirectional.txt", results.prefix))
