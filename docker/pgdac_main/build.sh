f1a1fe29aa0f2727176855beb84b66529a73b037
D. R. Mani <manidr@broadinstitute.org>
2018-09-14 16:09:54 -0400

    Modified pgdac_main docker to include missing libraries and update R-utilities

--- docker/pgdac_main/build.sh
+++ docker/pgdac_main/build.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-ver=1
+ver=2
 name=`basename $PWD`
 docker_tag=broadcptac/$name:$ver
 
 # cp from the mac results in fchmod permission denied errors -- ignore those and continue
 cp -r ../../src . || true
 rm -rf src/.Rproj* src/*.Rproj
+dos2unix src/*
+
 cp -r ../../data . || true
+cp -r ../packages . || true
+
+# also update R-utilities to include changes without having to rebuild r-util
+mkdir R-utilities
+cp ../../../R-utilities/*.[rR] R-utilities || true
+cp -r ../../../R-utilities/GSEA R-utilities || true
+
 docker build --rm -t $docker_tag .
 
 rm -rf src
 rm -rf data
+rm -rf packages
+rm -rf R-utilities
