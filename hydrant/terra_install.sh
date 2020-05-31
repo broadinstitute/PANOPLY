c130506b6f80462ba936e504ecf849652842cfaa
Ramani Kothadia <rbk301@nyu.edu>
2020-05-31 12:35:54 -0400

    Script to install WDLs to Terra

---
+++ hydrant/terra_install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ns="broadcptacdev"
+
+install_task_wdl(){
+  task=$1
+  hydrant install -m $task \
+    -n $ns \
+    -d tasks/$task/$task.wdl
+}
+
+install_wkflow_wdl(){
+  wkflow=$1
+  hydrant install -m $wkflow \
+    -n $ns \
+    -d workflows/$wkflow/$wkflow.wdl
+}
+
+
+# Uncomment for execution
+# -------------------------------------
+
+# install_task_wdl panoply_accumulate
+# install_task_wdl panoply_association
+# install_task_wdl panoply_cna_correlation
+# install_task_wdl panoply_cna_setup
+# install_task_wdl panoply_download
+# install_task_wdl panoply_harmonize
+# install_task_wdl panoply_normalize_ms_data
+# install_task_wdl panoply_parse_sm_table
+# install_task_wdl panoply_rna_protein_correlation
+# install_task_wdl panoply_sampleqc
+
+# install_wkflow_wdl unified_panoply
