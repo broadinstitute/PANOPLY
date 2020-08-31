761a14c3522d24e370aa5fe1b69c1b894d23fd4d
D. R. Mani <manidr@broadinstitute.org>
2020-08-31 09:52:31 -0400

    Setting up release scripts

--- release/setup-release.sh
+++ release/setup-release.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-panoply=`pwd`
+start_dir=`pwd`    # must be invoked from the PANOPLY/release directory
+panoply=$release_dir/..
 red='\033[0;31m'
 grn='\033[0;32m'
 reg='\033[0m'
 err="${red}error.${reg}"
 not="${grn}----->${reg}" ## notification
+
+base_url="https://registry.hub.docker.com/v2/repositories/"
 
 display_usage() {
   echo "usage: ./release.sh -p pull_dns -T release_tag "
@@ -16,6 +19,18 @@ display_usage() {
   exit
 }
 
+# replace the docker namespace and docker tag in the existing WDL
+# to the specified docker namespace and docker tag
+replaceDockerInWdl() {
+  task=$1
+  new_ns=$2
+  new_tag=$3
+  wdl_dns=`grep "/$task:" $task.wdl | cut -d'"' -f2 | cut -d'/' -f 1`
+  wdl_tag=`grep "/$task:" $task.wdl | cut -d'"' -f2 | cut -d':' -f 2`
+  sed -i '' "s|$wdl_dns/$task:$wdl_tag|$new_ns/$task:$new_tag|g" $task.wdl
+}
+
+
 while getopts ":p:T:N:h" opt; do
     case $opt in
         p) pull_dns="$OPTARG";;
@@ -25,9 +40,6 @@ while getopts ":p:T:N:h" opt; do
         \?) echo "Invalid Option -$OPTARG" >&2;;
     esac
 done
-
-base_url="https://registry.hub.docker.com/v2/repositories/"
-modules=( $( ls -d $panoply/tasks/panoply_* | xargs -n 1 basename ) )
 
 if [[ -z $pull_dns ]]; then
   echo -e "$err Development Docker Namespace required. Exiting.."
@@ -43,12 +55,15 @@ if [[ -z $release_dns ]]; then
   release_dns=$pull_dns
 fi
 
-mkdir -p releases;
+modules=( $( ls -d $panoply/tasks/panoply_* | xargs -n 1 basename ) )
+release_dir=version-$release_tag
+
+mkdir -p $release_dir;
 yes | docker system prune --all
 
 for mod in "${modules[@]}"
 do
-  mkdir -p releases/$mod
+  mkdir -p $release_dir/$mod
   url=$base_url$pull_dns/$mod/tags
   lat=( $( curl -s -S "$url" | \
              jq '."results"[]["name"]' | \
@@ -57,12 +72,24 @@ do
     continue;
   fi
   
-  mkdir -p releases/$mod;
-  cd releases/$mod;
+  mkdir -p $release_dir/$mod;
+  cd $release_dir/$mod;
+  
+  # build release docker
   echo -e "FROM $pull_dns/$mod:$lat" > Dockerfile
   docker build --rm --no-cache -t $release_dns/$mod:$release_tag . ;
   docker images | grep "$mod"
   docker login
   docker push $release_dns/$mod:$release_tag
-  cd $panoply;
+  
+  # copy and update task WDL
+  wdl_dir=$panoply/tasks/panoply_$mod/panoply_$mod
+  wdl=panoply_$mod.wdl
+  if [[ -f $wdl_dir/$wdl ]]; then
+    cp $wdl_dir/$wdl $wdl
+    replaceDockerInWdl panoply_$mod $release_dns $release_tag
+    # install method with synopsis/documentation
+    # get method snapshot id using fissfc meth_list and save release snapshot ids
+  fi
+  cd $start_dir;
 done
