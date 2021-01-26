32b4f7522815710ed1ca7e98e966970c8cee2ff9
D. R. Mani <manidr@broadinstitute.org>
2021-01-26 14:00:27 -0500

    Finalizing code and WDL for PTM normalization module

--- hydrant/tasks/panoply_ptm_normalization/panoply_ptm_normalization.wdl
+++ hydrant/tasks/panoply_ptm_normalization/panoply_ptm_normalization.wdl
@@ -10,9 +10,13 @@ task panoply_ptm_normalization {
     File proteome_gct
     File ptm_gct
     File yaml
-    String output_prefix
+    
+    String? output_prefix = basename (ptm_gct, ".gct")
 
-    String? sm_output
+    String? accession_number_col
+    String? accession_numbers_col
+    String? accession_numbers_sep
+    String? score_col
 
     command {
         set -euo pipefail
@@ -20,23 +24,27 @@ task panoply_ptm_normalization {
         Rscript /prot/proteomics/Projects/PGDAC/src/parameter_manager.r \
         --module ptm_normalization \
         --master_yaml ${yaml} \
-        ${"--spectrum_mill_output " + sm_output}
+        ${"--accession_number_colname " + accession_number_col} \
+        ${"--accession_numbers_colname " + accession_numbers_col} \
+        ${"--accession_numbers_separator " + accession_numbers_sep} \
+        ${"--score_colname" + score_col}
+
 
         Rscript /prot/proteomics/Projects/PGDAC/src/normalize-ptm.R \
           ${proteome_gct} \
           ${ptm_gct} \
-          ${default="NULL" output_prefix} \
-          ${yaml}
+          ${output_prefix} \
+          "final_output_params.yaml"
     }
 
     output {
-        File tar_out = "${output_prefix}_blacksheep.tar"
+        File tar_out = "${output_prefix}-proteome-relative-norm.gct"
     }
 
     runtime {
-        docker : "broadcptacdev/panoply_blacksheep:latest"
-        memory : select_first ([memory, 10]) + "GB"
-        disks : "local-disk " + select_first ([disk_space, 20]) + " SSD"
+        docker : "broadcptacdev/panoply_ptm_normalization:latest"
+        memory : select_first ([memory, 48]) + "GB"
+        disks : "local-disk " + select_first ([disk_space, 60]) + " SSD"
         cpu : select_first ([num_threads, 1]) + ""
         preemptible : select_first ([num_preemptions, 0])
     }
@@ -48,7 +56,6 @@ task panoply_ptm_normalization {
 }
 
 
-workflow panoply_blacksheep_workflow {
-    call panoply_blacksheep
-        
+workflow panoply_ptm_normalization_workflow {
+    call panoply_ptm_normalization
 }
--- src/panoply_ptm_normalization/normalize-ptm.R
+++ src/panoply_ptm_normalization/normalize-ptm.R
@@ -10,12 +10,6 @@
 # Fits PTM = beta_0 + beta_1*protein to all matched points in a dataset, and returns residuals as
 # protein-corrected PTM values.
 #
-# The try.all.accession.numbers option will attempt to match all PTM sites with underlying proteins
-# using alternative accession numbers.  If there is no initial match in the proteome to a PTM site,
-# the function will try all accession numbers associated with the PTM site and pick whichever match
-# (if there is one) with the highest protein score. This option is SpectrumMill specific --  PTM GCT 
-# must have 'accession_numbers' row metadata and proteome GCT must have 'scoreUnique' row metadata.
-#
 #########################################################################################################
 
 
@@ -23,29 +17,38 @@ library('pacman')
 p_load(cmapR)
 p_load(reshape)
 p_load(yaml)
+p_load(tidyr)
 
 
 normalize_ptm <- function (proteome.gct, ptm.gct, output.prefix=NULL, 
-                           try.all.accession.numbers=FALSE, ndigits=5)
+                           try.all.accession.numbers=TRUE,        # try hard to find a match (using accession_numbers)
+                           accession_number='accession_number',   # column with protein/PTM accession number
+                           accession_numbers='accession_numbers', # accession_numbers for protein/PTM group
+                           accession_sep='|',                     # spearator for each accession number in accession_numbers
+                           score='scoreUnique',                   # column with protein scores
+                           ndigits=5)
 {
   # import GCT files
   proteome <- parse.gctx (proteome.gct)
   PTM <- parse.gctx (ptm.gct)
 
-  # Spectrum Mill specific:
+  # Spectrum Mill specific (extensible to other search engines if function arguments are set appropriately):
   # if PTM accession number does not have match in the proteome, will try to match all accession numbers
-  # for that site in the SM output to the proteome. if there are multiple protein matches, it will pick
-  # the highest scoring one. replaces value in the 'accession_number' column of the PTM GCT with the new
+  # for that site in the proteome. if there are multiple protein matches, it will pick
+  # the highest scoring one (if score column present) or the first one (if score column absent).
+  # if this does not result in a match, search is extended to include all proteome accession numbers.
+  # replaces value in the 'accession_number' column of the PTM GCT with the new
   # match (duplicates original accession number column so those values are saved).
-  if (try.all.accession.numbers){
-    original_accession_number <- PTM@rdesc$accession_number
+  if (try.all.accession.numbers && !is.null (accession_numbers)) {
+    original_accession_number <- PTM@rdesc[,accession_number]
     PTM@rdesc <- data.frame (PTM@rdesc, original_accession_number, stringsAsFactors = FALSE)
-    PTM@rdesc <- swap.accession.numbers (PTM@rdesc, proteome@rdesc)
+    PTM@rdesc <- swap.accession.numbers (PTM@rdesc, proteome@rdesc, accession_number,
+                                         accession_numbers, accession_sep, score)
   }
   
   
   # fits linear model and returns updated GCT
-  PTM.norm <- normalize(PTM, proteome)
+  PTM.norm <- normalize(PTM, proteome, accession_number)
   
   
   # writes and returns updated GCT
@@ -60,18 +63,30 @@ normalize_ptm <- function (proteome.gct, ptm.gct, output.prefix=NULL,
 
 
 
-swap.accession.numbers <- function (PTM.rdesc, prot.rdesc) {
+swap.accession.numbers <- function (PTM.rdesc, prot.rdesc, accession_number,
+                                    accession_numbers, accession_sep, score)
+{
   # Matches PTM and proteome by accession number, and tries all possible PTM accession numbers if primary
   # accession number doesn't have proteome match
+  prot.rdesc.expanded <- prot.rdesc %>% separate_rows(!!as.name (accession_numbers), sep=accession_sep)
+  
   for (row in c(1:nrow(PTM.rdesc))) {
-    match <- which (prot.rdesc$accession_number == PTM.rdesc$accession_number[row])
-    if (length(match) == 0) {
-      accession.numbers <- unlist(strsplit(PTM.rdesc$accession_numbers[row],'|', fixed = TRUE))
-      matches <- prot.rdesc[which (prot.rdesc$accession_number %in% accession.numbers), ]
+    match <- which (prot.rdesc[,accession_number] == PTM.rdesc[row, accession_number])
+    if(length(match) == 0){
+      # first try to match PTM accession_numbers to protein primary ID
+      accession.numbers <- unlist(strsplit(PTM.rdesc[row,accession_numbers], accession_sep, fixed = TRUE))
+      matches <- prot.rdesc[which(prot.rdesc[,accession_number] %in% accession.numbers),]
       
-      if (nrow(matches) > 1) {
-        best <- matches$accession_number[which(matches$scoreUnique == max(matches$scoreUnique, na.rm = TRUE))]
-        PTM.rdesc$accession_number[row] <- best
+      if (nrow(matches) >= 1) {
+        best_index <- ifelse (!is.null(score), which.max(matches[,score]), 1)
+        PTM.rdesc[row, accession_number] <- matches[best_index, accession_number]
+      } else {
+        # if the above fails, try to match PTM accession_numbers to all proteome accession_numbers
+        matches <- prot.rdesc.expanded[which(prot.rdesc.expanded[,accession_numbers] %in% accession.numbers),]
+        if (nrow(matches) >= 1) {
+          best_index <- ifelse (!is.null(score), which.max(matches[,score]), 1)
+          PTM.rdesc[row, accession_number] <- matches[,accession_number]
+        } 
       }
     }
   }
@@ -81,7 +96,7 @@ swap.accession.numbers <- function (PTM.rdesc, prot.rdesc) {
 
 
 
-normalize <- function (PTM, proteome) {
+normalize <- function (PTM, proteome, accession_number) {
   # Applies linear regression to correct PTM levels for underlying protein levels
   
   # warn if not all samples are matched
@@ -93,9 +108,9 @@ normalize <- function (PTM, proteome) {
   # create merged data table
   PTM.melt <- melt.gct (PTM)
   prot.melt <- melt.gct (proteome)
-  prot.melt.data.only <- data.frame (prot.melt$id.y, prot.melt$accession_number, prot.melt$value)
-  colnames (prot.melt.data.only) <- c ('id.y', 'accession_number', 'value.prot')
-  data <- merge (PTM.melt, prot.melt.data.only, by = c('id.y', 'accession_number'))
+  prot.melt.data.only <- data.frame (prot.melt$id.y, prot.melt[,accession_number], prot.melt$value)
+  colnames (prot.melt.data.only) <- c ('id.y', accession_number, 'value.prot')
+  data <- merge (PTM.melt, prot.melt.data.only, by = c('id.y', accession_number))
   
   # print metrics
   percent <-round (100*nrow(data)/nrow(PTM.melt), digits = 1)
@@ -127,6 +142,7 @@ normalize <- function (PTM, proteome) {
 }
 
 
+if (!interactive()) {
 ## call via command line
 ## usage: Rscript normalize-ptm.R <proteome.gct> <ptm.gct> <output.prefix> <yaml.file>
 
@@ -140,10 +156,19 @@ yaml_file <- args[4]
 
 # read yaml parameters
 yaml_params <- read_yaml (yaml_file)
-sm_output <- yaml_params$panoply_ptm_normalization$spectrum_mill_output
+accession_number_col <- yaml_params$panoply_ptm_normalization$accession_number_colname
+accession_numbers_col <- yaml_params$panoply_ptm_normalization$accession_numbers_colname
+accession_numbers_sep <- yaml_params$panoply_ptm_normalization$accession_numbers_separator
+score_col <- yaml_params$panoply_ptm_normalization$score_colname
 ndigits <- yaml_params$global_parameters$output_precision$ndigits
+
 
 # call ptm normalization
 normalize_ptm (proteome.gct=proteome_gct, ptm.gct=ptm_gct, output.prefix=output_prefix,
-               try.all.accession.numbers=ifelse (sm_output, TRUE, FALSE), ndigits=ndigits)
-
+               try.all.accession.numbers=ifelse (accession_numbers_col=="NULL", FALSE, TRUE), 
+               accession_number=accession_number_col,
+               accession_numbers=ifelse (accession_numbers_col=="NULL", NULL, accession_numbers_col),
+               accession_sep=accession_numbers_sep,
+               score=ifelse (score_col=="NULL", NULL, score_col),
+               ndigits=ndigits)
+}
