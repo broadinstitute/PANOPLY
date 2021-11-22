8e7bf65c88ddcc1e74d489d5585dab9fb148ddf6
Munchic <khoi.u.pham@gmail.com>
2021-11-22 15:26:12 -0500

    ptm-sea docker 0.2.3

--- ptm-sea-notebook/terra-functions.R
+++ ptm-sea-notebook/terra-functions.R
@@ -3,16 +3,28 @@ project <- Sys.getenv('WORKSPACE_NAMESPACE')
 workspace <- Sys.getenv('WORKSPACE_NAME')
 bucket <- Sys.getenv('WORKSPACE_BUCKET')
 
-init_project_dir <- function() {
+init_project_dir <- function(name = NULL) {
     data_dir <<- file.path(MAIN_WD, "data")
-    project_input <<- file.path(data_dir, PROJECT_NAME, "input")
-    project_output <<- file.path(data_dir, PROJECT_NAME, "output")
+    project_name <<- get_run_name(name)
+    project_dir <<- file.path(data_dir, project_name)
+    project_input <<- file.path(project_dir, "input")
+    project_output <<- file.path(project_dir, "output")
 
     dir.create(data_dir, showWarnings = FALSE)
-    dir.create(file.path(data_dir, PROJECT_NAME), showWarnings = FALSE)
+    dir.create(file.path(data_dir, project_name), showWarnings = FALSE)
 
     dir.create(project_input, showWarnings = FALSE)
     dir.create(project_output, showWarnings = FALSE)
+}
+
+get_run_name <- function(name = NULL) {
+    if (!is.null(name)) {
+        run_name <- name
+    } else {
+        run_name <- format(Sys.time(), "%y%m%d-%H%M%S", tz = "EST")
+    }
+    
+    return(run_name)
 }
 
 list_files_in_bucket <- function(only_gct = FALSE) {
@@ -68,7 +80,7 @@ preprocess_gct <- function() {
     input_ds_proc <<- file.path(project_output, system(paste("cat", fn), intern = TRUE))  # locate processed GCT file
 }
 
-run_ptm_sea <- function() {
+run_ptm_sea <- function(save_to_bucket = TRUE, name = NULL) {
     setwd(project_output)  # change to folder to write
 
     sys_out <- system(paste("~/ssgsea-cli.R",
@@ -90,15 +102,26 @@ run_ptm_sea <- function() {
 
     print(sys_out)
     setwd(MAIN_WD)  # change to normal work directory
+    
+    if (save_to_bucket) {
+        save_results_to_bucket(name)
+    }
 }
 
-save_results_to_bucket <- function() {
-    setwd(project_output) 
-    setwd("../")
+save_results_to_bucket <- function(name = NULL) {
+    setwd(project_dir) 
+    
+    if (!is.null(name)) {
+        output_zip <- paste0(name, ".zip")
+    } else {
+        output_name <- paste0(workspace, "_", project_name)
+        output_zip <- paste0(output_name, ".zip")
+    }
 
-    output_zip <- paste0(PROJECT_NAME, ".zip")
-    sys_out <- system(paste("zip -r", output_zip, "output"), intern = TRUE)
+    sys_out <- system(paste("zip -r", output_zip, "output"), intern = TRUE, ignore.stdout = TRUE)
+    print(paste("PTM-SEA outputs compressed:", file.path(project_dir, output_zip)))
     system(paste("gsutil cp", output_zip, file.path(bucket, output_zip)), intern = TRUE)
+    print(paste("Output zip copied to the bucket:", output_zip))
 
     print(sys_out)
     setwd(MAIN_WD)  # change to normal work directory
