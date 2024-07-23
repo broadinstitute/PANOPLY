716f018a3b3c3812d7c5866d1522f11212634d4e
wcorinne <wcorinne@broadinstitute.org>
2024-07-23 16:10:12 -0400

    Added refmet.csv and some ID updates (which will probably need to be removed later lol)

--- src/panoply_metaboanalyst/metaboanalyst.R
+++ src/panoply_metaboanalyst/metaboanalyst.R
@@ -30,7 +30,8 @@ option_list <- list(
 #### Parse Command-Line Arguments ####
 opt_cmd <- parse_args( OptionParser(option_list=option_list),
                        # for testing arguments
-                       args = c('--metabolome_gct',"opt/input/ODG-v2_2-metabolomics_log_norm.gct",
+                       args = c('--metabolome_gct',"opt/input/ODG-v2_2-metabolomics-with-NMF.gct",
+                                #'--metabolome_gct',"opt/input/ODG-v2_2-metabolomics_log_norm.gct",
                                 '--contrast_gct',"opt/input/contrasts/metabolomics_logNorm-NMF.consensus-contrast.gct",
                                 '--proteome_gct',"opt/input/ODG-v2_2-proteome-SpectrumMill-ratio-QCfilter-NArm-with-NMF.gct",
                                 '--rna_gct',"opt/input/ODG-v2_2-rnaseq-expression-TPM-protein-coding-log2-median-norm-NArm-with-NMF.gct",
@@ -42,7 +43,8 @@ opt = opt_cmd # ToDo: Add YAML parameters (temporarily setting opt straight from
 
 
 
-library(MetaboAnalystR)
+library(MetaboAnalystR) # analysis tools for Metabolomic Data
+library(RefMet) # mapping between RefMet and other ID types
 library(glue)
 library(cmapR)
 library(tidyverse)
@@ -54,11 +56,16 @@ gct_rna = parse_gctx(opt$rna_gct)
 gct_contr = parse_gctx(opt$contrast_gct)
 
 enrichementAnalysis <- function(gct, cov_of_interest, value_of_interest, label,
+                                id_type = "name", pathway = "smpdb", match_no_hits = FALSE,
+                                rowNorm="NULL", # Select the option for row-wise normalization, "QuantileNorm" for Quantile Normalization, "CompNorm" for Normalization by a reference feature, "SumNorm" for Normalization to constant sum, "MedianNorm" for Normalization to sample median, and "SpecNorm" for Normalization by a sample-specific factor.
+                                transNorm="NULL", # Select option to transform the data, "LogNorm" for Log Normalization, and "CrNorm" for Cubic Root Transformation.
+                                scaleNorm="NULL", # Select option for scaling the data, "MeanCenter" for Mean Centering, "AutoNorm" for Autoscaling, "ParetoNorm" for Pareto Scaling, amd "RangeNorm" for Range Scaling.
                                 metabolite_id_col="Metabolite", sample_id_col="Sample.ID") {
   # write .txt file for Read.TextData() to parse
   df = t(gct@mat) %>% # transform matrix to samples as rows
     as.tibble(df) %>% # convert to tibble to prevent special-character replacement
-    set_names(gct@rdesc[[metabolite_id_col]]) %>% # set colnames as original Metabolite IDs
+    set_names(gct@rid) %>% # set colnames as original Metabolite IDs
+    # set_names(gct@rdesc[[metabolite_id_col]]) %>% # set colnames as original Metabolite IDs
     mutate(Sample.ID = gct@cdesc[[sample_id_col]]) %>% # add Sample.ID as column
     mutate(cls = gct@cdesc[[cov_of_interest]]) %>%
     mutate(cls = ifelse(cls==value_of_interest, cls, glue("not_{value_of_interest}"))) %>% # add Sample.ID as column
@@ -71,9 +78,9 @@ enrichementAnalysis <- function(gct, cov_of_interest, value_of_interest, label,
   mSet <- Read.TextData(mSet, "tmp.csv", "rowu", "disc")
   
   # Perform cross-referencing of compound names
-  mSet <- CrossReferencing(mSet, "name");
+  mSet <- CrossReferencing(mSet, id_type, lipid=F);
   
-  match_no_hits=FALSE # toggle for metabolites without hits should be soft-searched against MetaboAnalyst's databases
+  # match_no_hits=FALSE # toggle for metabolites without hits should be soft-searched against MetaboAnalyst's databases
   no_hits = mSet$name.map$query.vec[!mSet$name.map$match.state] # get list of metabolites with no hits
   if (length(no_hits)>0 && match_no_hits) { for (id in no_hits) {
     cat(glue("\n\n#####\nMetabolite '{id}' has the following potential matches:\n\n"))
@@ -82,32 +89,33 @@ enrichementAnalysis <- function(gct, cov_of_interest, value_of_interest, label,
     cat(glue("\n\n#####\n{length(no_hits)} of {length(mSet$name.map$query.vec)} metabolites did not map to known compound names\n\n"))
   }
   
-  mSet<-CreateMappingResultTable(mSet) # not clear what this does
+  mSet<-CreateMappingResultTable(mSet) # creates the mSet$dataSet$map.table entry (map between ID types)
   
   
   #### Preprocessing Steps ####
   # Mandatory check of data 
   mSet1<-SanityCheckData(mSet);
   if (is.numeric(mSet1)) { stop("Error code in SanityCheckData().") } else { mSet <- mSet1 }
-  # Replace missing values with minimum concentration levels (required, even when unnecessary)
+  # Replace missing values with minimum concentration levels (required, even when no missing exist)
+  # ToDo: NOTE that this replaces things with half the smallest positive value-- it ASSUMES we have all positive, which doesn't work for z-scores 
   mSet<-ReplaceMin(mSet);
   # Perform no normalization
   mSet<-PreparePrenormData(mSet)
-  mSet<-Normalization(mSet, "NULL", "NULL", "NULL", "PIF_178", ratio=FALSE, ratioNum=20)
+  mSet<-Normalization(mSet, rowNorm=rowNorm, transNorm=transNorm, scaleNorm=scaleNorm, ratio=FALSE, ratioNum=20)
   
   
   #### Normalization Plots ####
   # Plot normalization
-  mSet<-PlotNormSummary(mSet, "norm_0_", "png", 72, width=NA)
+  mSet<-PlotNormSummary(mSet, glue("{label}_norm_0_"), "png", 72, width=NA)
   # Plot sample-wise normalization
-  mSet<-PlotSampleNormSummary(mSet, "snorm_0_", "png", 72, width=NA)
+  mSet<-PlotSampleNormSummary(mSet, glue("{label}_snorm_0_"), "png", 72, width=NA)
   
   
   #### Finalize Enrichement Setup & Visualize ####
   # Set the metabolome filter
   mSet<-SetMetabolomeFilter(mSet, F); # turn metabolite filter off
   # Set the metabolite set library to pathway
-  mSet<-SetCurrentMsetLib(mSet, glue("{label}_smpdb_pathway"), 0); # might be able to change pathways here
+  mSet<-SetCurrentMsetLib(mSet, glue("{pathway}_pathway"), 0); # might be able to change pathways here
   # Calculate the global test score
   mSet<-CalculateGlobalTestScore(mSet)
   # Plot the QEA
@@ -123,13 +131,29 @@ enrichementAnalysis <- function(gct, cov_of_interest, value_of_interest, label,
 
 
 # variables for enrichement analysis
-gct = gct_meta
+gct_full = gct_meta
 metabolite_id_col = "Metabolite"
 sample_id_col = "Sample.ID"
 cov_of_interest = "Type"
+pathway = "kegg"
+id_type = "name"
 
+#### ID Mapping ####
+gct_full@rdesc$Standardized.name = refmet_map_df(gct_full@rdesc[[metabolite_id_col]])$Standardized.name # get standardized names with RefMet::refmet_map_df(), and add to rdesc
+refmet_map = read.csv(file.path(opt$lib_dir, 'refmet.csv'))
+gct_full@rdesc = left_join(gct_full@rdesc, refmet_map, by = c("Standardized.name" = "refmet_name"))
+# gct_full@rdesc$Metabolite[which(gct_full@rdesc$Standardized.name=="-")] # get IDs that didn't map
+# prune no-match or duplicates IDs
+# duplicated_pubchem_id = unique(gct_full@rdesc$pubchem_cid[which(duplicated(gct_full@rdesc$pubchem_cid))])
+# gct = subset_gct(gct_full, rid = which(!(gct_full@rdesc$pubchem_cid %in% duplicated_pubchem_id)))
+# metabolite_id_col = "pubchem_cid"
+# id_type = "pubchem"
+
+#### Enrichement Analysis ####
 for (value_of_interest in unique(gct@cdesc[[cov_of_interest]])) {
-  enrichementAnalysis(gct, cov_of_interest, value_of_interest, label = glue("{opt$output_prefix}_{value_of_interest}"))
+  enrichementAnalysis(gct_full, cov_of_interest, value_of_interest, id_type = id_type,
+                      # metabolite_id_col = 'Standardized.name',
+                      label = glue("{opt$output_prefix}_{value_of_interest}"), pathway=pathway)
 }
 
 
